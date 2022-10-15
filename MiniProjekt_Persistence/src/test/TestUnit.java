4405255ea435a7177e371948e5f51c53ef164dd9
Laurits Neimann Søgaard <10414004@ucn.dk>
2022-10-15 14:46:50 +0200

    Update test done

--- MiniProjekt_Persistence/src/test/TestUnit.java
+++ MiniProjekt_Persistence/src/test/TestUnit.java
@@ -8,15 +8,19 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import database.PersonDB;
 import database.ProductDB;
 import model.BuyProduct;
 import model.Clothing;
+import model.Customer;
 import model.Product;
 import model.Supplier;
 import utility.DataAccessException;
 
 class TestUnit {
 	ProductDB productDB;
+	PersonDB personDB;
+	
 
 
 	@Test
@@ -34,8 +38,31 @@ class TestUnit {
 	} 
 	
 	@Test
-	public void findCustomerOnPhone() {
-		
+	public void findCustomerOnPhone() throws DataAccessException {
+		// Arrange
+		personDB = new PersonDB();
+		String phone = "+4512341234";
+		// Act
+		Customer customer = personDB.findCustomerByPhone(phone);
+		// Assert
+		assertEquals("Per", customer.getFname());
+		assertEquals("Svendson", customer.getLname());
+		assertEquals("Billegade 10", customer.getAddress());
+		assertEquals(9000, customer.getZipcode());
+		assertEquals("Aalborg", customer.getCity());
+		assertEquals("Person1@mail.com", customer.getEmail());
+		assertEquals(phone, customer.getPhone());
+	}
+	
+	@Test
+	public void findCustomerOnPhoneNegative() throws DataAccessException {
+		// Arrange
+		personDB = new PersonDB();
+		String phone = "+4512341232";
+		// Act
+		Customer customer = personDB.findCustomerByPhone(phone);
+		// Assert
+		assertNull(customer.getPhone());
 	}
 
 }
